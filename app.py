116360e7799b6f6130a1b8010c29c12bfdaafa32
solodant <>
2025-05-16 21:57:28 +0300

    app.py modified

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
+import pandas as pd
 from cli.parser import parse_arguments
 from services.data_service import DataService
 from services.analysis import AnalysisService
 from services.visualization import VisualizationService
 
+
 def main():
-    """Orchestrate the application workflow."""
     args = parse_arguments()
     data, title = DataService.load_data(args)
     analysis = AnalysisService.analyze(data)
     VisualizationService.show(data['Close'], analysis, title)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
